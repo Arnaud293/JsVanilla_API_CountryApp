0b51015e2a3ebbe2873944ece99f54da7206cae1
Arnaud293 <arnaudlt293@gmail.com>
2022-09-30 16:04:07 +0200

    first fetch, start display function

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// coutry.name.includes(inputSearch.value);
+const container = document.querySelector('.countries-container');
+let countries = [];
 
-coutry.name.includes(inputSearch.value);
+const fetchCountries = async () => {
+    await fetch('https://restcountries.com/v3.1/all')
+    .then((res) => res.json())
+    .then((data) => (countries = data));
+
+    console.log(countries)
+    CountriesDisplay();
+}
+
+fetchCountries();
+
+const CountriesDisplay = () => {
+
+    for(let i = 0; i < countries.length; i++){
+
+        container.innerHTML = countries.map((country) => {
+            return  ` 
+                <ul>
+                    <li>
+                        <h2>${country.name.common}</h2>
+                    </li>
+                </ul>`
+        }).join('');
+    }
+}
 
